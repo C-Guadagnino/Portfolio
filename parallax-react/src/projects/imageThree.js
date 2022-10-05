6a73060915d1f2001a5e9d5a8cb92addad14e8d1
Cameron Guadagnino <cjguad@gmail.com>
2022-10-04 17:19:24 -0700

    Added photos to the readme

--- parallax-react/src/projects/imageThree.js
+++ parallax-react/src/projects/imageThree.js
@@ -1,8 +1,9 @@
+import { useReducer } from 'react';
 import { Parallax } from 'react-parallax';
 import image2 from "../images/RM_3_4.png"
 
 const ImageThree = () => (
-    <Parallax className='image' bgImage={image2} strength={800}>
+    <Parallax className='image' bgImage={user.profilePicture.get()} strength={800}>
         <div className='content'>
             <span className='image-txt'> Your next Culinary Creation</span>
         </div>
